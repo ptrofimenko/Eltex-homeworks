a7f6210df538cb16f7b1f30b2a296704a015d09c
ptrofimenko <42354842+ptrofimenko@users.noreply.github.com>
2018-12-10 00:36:45 +0700

    Add files via upload

---
+++ my_little_firewall/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+sudo make -C /usr/src/linux-headers-4.15.0-29-generic M=$PWD clean
+#sudo make -C /usr/src/linux-headers-4.15.0-36 M=$PWD modules
+
---
+++ my_little_firewall/make.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+sudo make -C /usr/src/linux-headers-4.15.0-29-generic M=$PWD modules
+#sudo make -C /usr/src/linux-headers-4.15.0-36 M=$PWD modules
+
